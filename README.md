9996b9b0ef4e81a951c423c56d2623b6b8289fd3
majdabouhashish <majdabouhashish@gmail.com>
2022-05-16 12:42:40 +0300

    v0.3 'Send One to Multiple emails with an attachment'

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-- V0.2: one email sender with an attachment
+#Send One to Multiple emails with an attachment by one click at once without BCC or the recipients to know you send to other people.
+
+- V0.3: one to multiple email sender with an attachment, emails will be read from a text file.
 - the dependencies in pom.xml file must exits.
-- You need to access this link: https://myaccount.google.com/lesssecureapps, from the email ur sending the message from, and turn on "Less secure app access".
+- You need to access this link: https://myaccount.google.com/lesssecureapps, from the email ur sending the message from, and turn on "Less secure app access", you can turn it off, once you send the emails.
+- What you need to update in the code (sendEmails.java) to work with you:
+  - You need to update the sender email ("from" variable).
+  - You need to update the sender password ("password_sender" variable).
+  - You need to update the emails you will send to in src//main//resources//emails.txt.
+  - You need to update the email header.
+  - You need to update the message.
+  - You need to update the path for the attachment file.
+
 - Original Reference: https://netcorecloud.com/tutorials/send-email-in-java-using-gmail-smtp/
--- src/main/java/sendEmails.java
+++ src/main/java/sendEmails.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties; //represents a persistent set of properties. The Properties can be saved to a stream or loaded from a stream.
 import javax.mail.*;
 import javax.mail.internet.InternetAddress; //This class represents an Internet email address using the syntax of RFC822
@@ -6,82 +8,90 @@ import javax.mail.internet.MimeMultipart; //This Class is an implementation of t
 import javax.mail.internet.MimeBodyPart; //This class represents a MIME body part. MimeBodyPart uses the InternetHeaders class to parse and store the headers of that body part.
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class sendEmails {
     public static void main(String[] args) {
-
-        // Recipient's email ID needs to be mentioned.
-        String to = "sender@gmail.com";
-
-        // Sender's email ID needs to be mentioned
-        String from = "reciever@gmail.com";
-
-        // Assuming you are sending email from through gmails smtp
-        String host = "smtp.gmail.com";
-
-        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Setup mail server
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.auth", "true");
-
-        // Get the Session object (Session class represents a mail session).
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() { //(This class is simply a repository for a username and a password.)
-                return new PasswordAuthentication("sender@gmail.com", "senderPassword");  //Enter the email and password of the sender email
-            }
-        });
-
-        // Used to debug SMTP issues
-        session.setDebug(true);
-
         try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
+            Scanner emails = new Scanner(new FileInputStream("src//main//resources//emails.txt")); //the path of the emails list
+            while (emails.hasNextLine()) { //read the emails
 
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
+                String to = emails.nextLine(); // Recipient's email ID
 
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                if (!to.isEmpty()) { //to ignore any empty line in the text file.
 
-            // Set Subject: header field
-            message.setSubject("This is the Subject");
+                    // Sender's email ID needs to be mentioned
+                    String from = "sender@gmail.com";
+                    String password_sender = "password";
 
-            // Now set the actual message
-            message.setText("This is actual message"); //if u add an attachment this message will be ignored and the message will be the textPart
+                    // Assuming you are sending email from through gmails smtp
+                    String host = "smtp.gmail.com";
 
-            //add an attachment
-            Multipart multipart = new MimeMultipart();
+                    // Get system properties
+                    Properties properties = System.getProperties();
 
-            MimeBodyPart attachmentPart = new MimeBodyPart();
+                    // Setup mail server
+                    properties.put("mail.smtp.host", host);
+                    properties.put("mail.smtp.port", "465");
+                    properties.put("mail.smtp.ssl.enable", "true");
+                    properties.put("mail.smtp.auth", "true");
 
-            MimeBodyPart textPart = new MimeBodyPart();
+                    // Get the Session object (Session class represents a mail session).
+                    Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() { //(This class is simply a repository for a username and a password.)
+                            return new PasswordAuthentication(from, password_sender);  //Enter the email and password of the sender email
+                        }
+                    });
 
-            try {
+                    // Used to debug SMTP issues
+                    session.setDebug(true);
 
-                File f =new File("C:\\Users\\majds\\desktop\\moon.png"); //the path of the attachment file
+                    try {
+                        // Create a default MimeMessage object.
+                        MimeMessage message = new MimeMessage(session);
 
-                attachmentPart.attachFile(f);
-                textPart.setText("This is text");
-                multipart.addBodyPart(textPart);
-                multipart.addBodyPart(attachmentPart);
+                        // Set From: header field of the header.
+                        message.setFrom(new InternetAddress(from));
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                        // Set To: header field of the header.
+                        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+                        // Set Subject: header field
+                        message.setSubject("Email title");
+
+                        //add an attachment
+                        Multipart multipart = new MimeMultipart();
+
+                        MimeBodyPart attachmentPart = new MimeBodyPart();
+
+                        MimeBodyPart textPart = new MimeBodyPart();
+
+                        try {
+
+                            File f = new File("C:\\Users\\majds\\desktop\\moon.png"); //the path of the attachment file
+
+                            attachmentPart.attachFile(f);
+                            textPart.setText("Email message");
+                            multipart.addBodyPart(textPart);
+                            multipart.addBodyPart(attachmentPart);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        message.setContent(multipart);
+
+                        // Send message
+                        Transport.send(message);
+
+                    } catch (MessagingException mex) {
+                        mex.printStackTrace();
+                    }
+                }
             }
-
-            message.setContent(multipart);
-
-            // Send message
-            Transport.send(message);
-
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
         }
-
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
